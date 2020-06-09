faec7031077777238c3b060e45bb6803ef089d55
Santanu Das <santanud.411@gmail.com>
2020-06-09 21:49:25 +0530

    Source files uploaded
    Added basic functionality to return hash of a URL

---
+++ src/main/java/com/shrot/ShrotApplication.java
@@ -0,0 +1,13 @@
+package com.shrot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShrotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShrotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shrot/ShrotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shrot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShrotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
